41bd3b9fc9b914dcaad19283eb3d930d2a2c7844
tylercbreton <tcbreton@gmail.com>
2014-07-22 19:37:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Garageio::Application.config.secret_key_base = '61898a85897b7dd32dd8436eaa54cc3d632f3ee419373f97e5b15f59bf720e3c2ac9ad07e72d9242ac336cc99d63e5575ec1e02cddc23fe8f01495d900819a46'
